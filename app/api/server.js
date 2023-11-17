ea198f52d6dd08cffd87b5533f3e0b635063e9bc
Salim Ali <u14262208@tuks.co.za>
2023-11-17 10:47:08 +0200

    removed boilerplate

---
+++ app/api/server.js
@@ -0,0 +1,8 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+
+
+export default function handler(request, response) {
+
+  response.status(200).json({ name: "Salim Ali" });
+
+}
